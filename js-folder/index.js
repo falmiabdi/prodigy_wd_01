d78dd0155b1d3964cf8345c1a309c4e9e770d23f
falmiabdi <falmitesfaye@gmail.com>
2025-03-03 14:26:34 -0800

    the first Landing page

--- js-folder/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const mobileMenu = document.getElementById("mobile-menu");
-  const navList = document.getElementById("nav-list");
-
-  mobileMenu.addEventListener("click", function () {
-    navList.classList.toggle("active"); // Toggle the 'active' class to show/hide the menu
-  });
-});
